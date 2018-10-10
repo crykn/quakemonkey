80278f638ab8a9230da6c17fd10075b59a738fcf
crykn <crykn@users.noreply.github.com>
2018-10-10 14:22:22 +0200

    Added more tests.

---
+++ src/test/java/net/namekdev/quakemonkey/messages/MessagesTests.java
@@ -0,0 +1,30 @@
+package net.namekdev.quakemonkey.messages;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MessagesTests {
+
+	@Test
+	public void testToString() {
+		// AckMessage
+		AckMessage ackMessage = new AckMessage();
+		ackMessage.set((short) 3);
+		assertEquals("AckMessage { id: 3}", ackMessage.toString());
+
+		// DiffMessage
+		DiffMessage diffMessage = new DiffMessage();
+		diffMessage.set((short) 5, new byte[] { (byte) 12 },
+				new int[] { 3, 6, 88 });
+		assertEquals("DiffMessage { id: 5, flags: [12], data: [3, 6, 88]}",
+				diffMessage.toString());
+
+		// LabeledMessage
+		LabeledMessage labeledMessage = new LabeledMessage();
+		labeledMessage.set((short) 23, "testabc");
+		assertEquals("LabeledMessage { label: 23, payloadMessage: testabc}",
+				labeledMessage.toString());
+	}
+
+}
